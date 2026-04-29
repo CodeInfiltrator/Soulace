3dca2672f522d25ee1e0dee6aa3e104383b1d696
CodeInfiltrator <efdarrens@gmail.com>
2026-04-29 15:21:33 +0700

    Initial Commit

---
+++ Soulace/SoulaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulaceApp.swift
+//  Soulace
+//
+//  Created by Frizzia Darren on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SoulaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
