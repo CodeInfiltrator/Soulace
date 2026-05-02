483ac346e4629390cb7270ac8fd52d13e7e3c5da
LiecardoAntonio <liecardoantonio24@gmail.com>
2026-05-02 13:44:22 +0700

    test lie

--- Soulace/ContentView.swift
+++ Soulace/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Grace IJIN TESTING di main")
+            Text("lie IJIN TESTING di main")
         }
         .padding()
     }
